5a08e0736cfd53d665a93759fcca050d60e55374
Krishna <krishnagupta2531699@gmail.com>
2026-03-01 13:54:00 +0530

    edite

--- home page/home.js
+++ home page/home.js
@@ -1,3 +1,15 @@
 function browse(){
     alert("Browse Cars Section");
 }
+  const user = localStorage.getItem("userName");
+
+  if (user) {
+    document.getElementById("welcomeUser").innerText = "Welcome, " + user;
+    document.getElementById("loginBtn").style.display = "none";
+    document.getElementById("logoutBtn").style.display = "inline";
+  }
+
+  function logout() {
+    localStorage.clear();
+    location.reload();
+  }
