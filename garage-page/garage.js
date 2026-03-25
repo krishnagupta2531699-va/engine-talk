2aa9ec004ab98fbbf4c01345ec9b8311b10a23c8
Krishna <krishnagupta2531699@gmail.com>
2026-03-25 09:26:16 +0530

    a

--- garage-page/garage.js
+++
@@ -1,7 +0,0 @@
-let car = JSON.parse(localStorage.getItem("editCar"));
-
-if(car){
-document.getElementById("carName").innerText = car.name;
-document.getElementById("price").innerText = car.price;
-document.getElementById("carImage").src = car.image;
-}
